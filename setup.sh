186834b5480061bbe3f130f06333bca68d76800a
Simas Cepaitis <simas.cepaitis@pearson.com>
2014-02-16 15:57:25 +0000

    Add gitconfig

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc vim zshrc gemrc oh-my-zsh tmux.conf irssi"    # list of files/folders to symlink in homedir
+files="vimrc vim zshrc gemrc oh-my-zsh tmux.conf irssi gitconfig"    # list of files/folders to symlink in homedir
 
 ##########
 
